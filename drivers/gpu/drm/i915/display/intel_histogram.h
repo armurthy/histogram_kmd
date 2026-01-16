84275c53333a7d8313e0e5056899bb5e7a7fe0d4
Arun R Murthy <arun.r.murthy@intel.com>
2026-01-16 11:54:43 +0530

    drm/i915/histogram: Add support for histogram
    Statistics is generated from the image frame that is coming to display
    and an event is sent to user after reading this histogram data.
    
    v2: forward declaration in header file along with error handling (Jani)
    v3: Replaced i915 with intel_display (Suraj)
    v4: Removed dithering enable/disable (Vandita)
        New patch for histogram register definitions (Suraj)
    v5: IET LUT pgm follow the seq in spec and removed change to TC at end
        (Suraj)
    v8: Retained only the Histogram part and move IET LUT to a different
        patch.
    
    Signed-off-by: Arun R Murthy <arun.r.murthy@intel.com>

---
+++ drivers/gpu/drm/i915/display/intel_histogram.h
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+/*
+ * Copyright © 2025 Intel Corporation
+ */
+
+#ifndef __INTEL_HISTOGRAM_H__
+#define __INTEL_HISTOGRAM_H__
+
+#include <linux/types.h>
+#include <linux/workqueue.h>
+
+struct delayed_work;
+struct drm_property_blob;
+struct drm_histogram_config;
+struct drm_histogram_caps;
+struct intel_crtc;
+
+#define HISTOGRAM_BIN_COUNT                    32
+
+struct intel_histogram {
+	struct drm_histogram_caps *caps;
+	struct intel_crtc *crtc;
+	struct delayed_work work;
+	bool enable;
+	bool can_enable;
+	u32 bin_data[HISTOGRAM_BIN_COUNT];
+};
+
+enum intel_global_hist_status {
+	INTEL_HISTOGRAM_ENABLE,
+	INTEL_HISTOGRAM_DISABLE,
+};
+
+enum intel_global_histogram {
+	INTEL_HISTOGRAM,
+};
+
+enum intel_global_hist_lut {
+	INTEL_HISTOGRAM_PIXEL_FACTOR,
+};
+
+int intel_histogram_atomic_check(struct intel_crtc *intel_crtc);
+int intel_histogram_update(struct intel_crtc *intel_crtc,
+			   struct drm_histogram_config *config);
+int intel_histogram_init(struct intel_crtc *intel_crtc);
+void intel_histogram_finish(struct intel_crtc *intel_crtc);
+
+#endif /* __INTEL_HISTOGRAM_H__ */
